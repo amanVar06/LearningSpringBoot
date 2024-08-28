a8acbfb0cfde32071055f28fd97a90ad7da9ec8f
Aman Varshney <amanvarshney212@gmail.com>
2024-08-28 15:05:11 +0530

    Started New Project for Learning.

---
+++ src/main/java/com/amanv/myapp/Dev.java
@@ -0,0 +1,11 @@
+package com.amanv.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("Welcome to MyApp!");
+    }
+}
